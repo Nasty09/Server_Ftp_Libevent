125c7108f95a0b1996bcab0020df6c06355f322e
Anastasia Zaytseva <72304256+Nasty09@users.noreply.github.com>
2021-10-11 19:44:43 +0300

    Delete Ftp_User.cpp

--- Asinc_Server/Ftp_User.cpp
+++
@@ -1,6 +0,0 @@
-#include "Server.hpp"
-
-void Ftp_User::Parse(const std::string &type, const std::string &msg) {
-    std::cout << "XFTPUSER::Parse " << type << " " << msg << "\n";
-    ResponseCMD("230 Login successful.\r\n");
-}
